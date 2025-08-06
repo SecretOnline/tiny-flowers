777187e3b877be5224e6fa48a914fff6b002ebab
Erin Anderson <4420108+eSonOfAnder@users.noreply.github.com>
2025-08-05 22:35:49 -0700

    Updated to 1.21.8

--- src/client/java/co/secretonline/tinyflowers/TinyFlowersClient.java
+++ src/client/java/co/secretonline/tinyflowers/TinyFlowersClient.java
@@ -2,9 +2,10 @@ package co.secretonline.tinyflowers;
 
 import co.secretonline.tinyflowers.blocks.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.minecraft.client.color.world.BiomeColors;
+import net.minecraft.client.render.BlockRenderLayer;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.item.ItemRenderState;
 import net.minecraft.util.math.random.Random;
@@ -17,7 +18,7 @@ public class TinyFlowersClient implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
-		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.TINY_GARDEN, RenderLayer.getCutout());
+		BlockRenderLayerMap.putBlock(ModBlocks.TINY_GARDEN, BlockRenderLayer.CUTOUT);
 
 		// See Pink Petals in net.minecraft.client.color.block.BlockColors
 		ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> {
--- src/client/java/co/secretonline/tinyflowers/datagen/ItemTagProvider.java
+++ src/client/java/co/secretonline/tinyflowers/datagen/ItemTagProvider.java
@@ -8,19 +8,20 @@ import co.secretonline.tinyflowers.items.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.fabricmc.fabric.api.tag.convention.v2.ConventionalItemTags;
+import net.minecraft.data.tag.ProvidedTagBuilder;
 import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.ItemTags;
 
-public class ItemTagProvider extends FabricTagProvider<Item> {
+public class ItemTagProvider extends FabricTagProvider.FabricValueLookupTagProvider.ItemTagProvider {
 	public ItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
-		super(output, RegistryKeys.ITEM, registriesFuture);
+		super(output, registriesFuture);
 	}
 
 	@Override
 	protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
-		FabricTagBuilder builder = getOrCreateTagBuilder(ModItemTags.TINY_FLOWERS);
+		ProvidedTagBuilder builder = valueLookupBuilder(ModItemTags.TINY_FLOWERS);
 
 		// Add all items/blocks that correspond to tiny flower variants to tag
 		for (FlowerVariant variant : FlowerVariant.values()) {
@@ -31,8 +32,8 @@ public class ItemTagProvider extends FabricTagProvider<Item> {
 			builder.add(variant.asItem());
 		}
 
-		getOrCreateTagBuilder(ItemTags.BEE_FOOD).addTag(ModItemTags.TINY_FLOWERS);
+		valueLookupBuilder(ItemTags.BEE_FOOD).addTag(ModItemTags.TINY_FLOWERS);
 
-		getOrCreateTagBuilder(ConventionalItemTags.SHEAR_TOOLS).add(ModItems.FLORISTS_SHEARS_ITEM);
+		valueLookupBuilder(ConventionalItemTags.SHEAR_TOOLS).add(ModItems.FLORISTS_SHEARS_ITEM);
 	}
 }
