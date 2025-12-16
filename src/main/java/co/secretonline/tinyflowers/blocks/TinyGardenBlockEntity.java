fa055475b3b1381bfd75a3e76d3d8420f2c83577
secret_online <me@secretonline.co>
2025-12-16 21:07:18 +1300

    Implement most of the shears functionality back in
    This crashes, and I think it's because the registry doesn't exist on the client? No idea if that's actually the case, but it's getting late and I want to sleep.

--- src/main/java/co/secretonline/tinyflowers/blocks/TinyGardenBlockEntity.java
+++ src/main/java/co/secretonline/tinyflowers/blocks/TinyGardenBlockEntity.java
@@ -1,11 +1,18 @@
 package co.secretonline.tinyflowers.blocks;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jetbrains.annotations.Nullable;
 
+import co.secretonline.tinyflowers.data.TinyFlowerData;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.Identifier;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SegmentableBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.ValueInput;
@@ -27,40 +34,86 @@ public class TinyGardenBlockEntity extends BlockEntity {
 		super(ModBlockEntities.TINY_GARDEN_BLOCK_ENTITY, pos, state);
 	}
 
-	@Nullable
-	public Identifier getFlower1() {
-		return flower1;
-	}
+	public List<Identifier> getFlowers() {
+		List<Identifier> list = new ArrayList<>(NUM_SLOTS);
 
-	public void setFlower1(@Nullable Identifier flower1) {
-		this.flower1 = flower1;
-	}
+		if (flower1 != null) {
+			list.add(flower1);
+		}
+		if (flower2 != null) {
+			list.add(flower2);
+		}
+		if (flower3 != null) {
+			list.add(flower3);
+		}
+		if (flower4 != null) {
+			list.add(flower4);
+		}
 
-	@Nullable
-	public Identifier getFlower2() {
-		return flower2;
-	}
-
-	public void setFlower2(@Nullable Identifier flower2) {
-		this.flower2 = flower2;
-	}
-
-	@Nullable
-	public Identifier getFlower3() {
-		return flower3;
-	}
-
-	public void setFlower3(@Nullable Identifier flower3) {
-		this.flower3 = flower3;
+		return list;
 	}
 
 	@Nullable
-	public Identifier getFlower4() {
-		return flower4;
+	public Identifier getFlower(int index) {
+		switch (index) {
+			case 1:
+				return flower1;
+			case 2:
+				return flower2;
+			case 3:
+				return flower3;
+			case 4:
+				return flower4;
+			default:
+				throw new IndexOutOfBoundsException(index);
+		}
 	}
 
-	public void setFlower4(@Nullable Identifier flower4) {
-		this.flower4 = flower4;
+	public void setFlower(int index, @Nullable Identifier id) {
+		switch (index) {
+			case 1:
+				flower1 = id;
+				return;
+			case 2:
+				flower2 = id;
+				return;
+			case 3:
+				flower3 = id;
+				return;
+			case 4:
+				flower4 = id;
+				return;
+			default:
+				throw new IndexOutOfBoundsException(index);
+		}
+	}
+
+	public boolean addFlower(Identifier newId) {
+		if (flower1 == null) {
+			flower1 = newId;
+			return true;
+		}
+		if (flower2 == null) {
+			flower2 = newId;
+			return true;
+		}
+		if (flower3 == null) {
+			flower3 = newId;
+			return true;
+		}
+		if (flower4 == null) {
+			flower4 = newId;
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean isEmpty() {
+		return flower1 == null &&
+				flower2 == null &&
+				flower3 == null &&
+				flower4 == null;
 	}
 
 	@Override
@@ -86,5 +139,26 @@ public class TinyGardenBlockEntity extends BlockEntity {
 	@Override
 	public CompoundTag getUpdateTag(HolderLookup.Provider registryLookup) {
 		return saveWithoutMetadata(registryLookup);
+	}
+
+	public boolean setFromPreviousBlockState(RegistryAccess registryAccess, BlockState state) {
+		Block block = state.getBlock();
+
+		TinyFlowerData tinyFlowerData = TinyFlowerData.findByOriginalBlock(registryAccess, block);
+		if (tinyFlowerData == null) {
+			return false;
+		}
+
+		Identifier id = tinyFlowerData.id();
+		int amount = block instanceof SegmentableBlock segmentedBlock
+				? state.getValue(segmentedBlock.getSegmentAmountProperty())
+				: NUM_SLOTS;
+
+		setFlower(1, amount >= 1 ? id : null);
+		setFlower(2, amount >= 2 ? id : null);
+		setFlower(3, amount >= 3 ? id : null);
+		setFlower(4, amount >= 4 ? id : null);
+
+		return true;
 	}
 }
