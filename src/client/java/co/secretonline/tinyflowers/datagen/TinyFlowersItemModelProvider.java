1230524f39bd64becdf5e82ce63bd01815ac3d63
secret_online <me@secretonline.co>
2025-12-16 17:00:35 +1300

    Add texture fallback for tiny flowers with an invalid component

--- src/client/java/co/secretonline/tinyflowers/datagen/TinyFlowersItemModelProvider.java
+++ src/client/java/co/secretonline/tinyflowers/datagen/TinyFlowersItemModelProvider.java
@@ -3,6 +3,7 @@ package co.secretonline.tinyflowers.datagen;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import co.secretonline.tinyflowers.TinyFlowers;
 import co.secretonline.tinyflowers.components.TinyFlowerComponent;
 import co.secretonline.tinyflowers.data.TinyFlowerData;
 import co.secretonline.tinyflowers.items.ModItems;
@@ -15,7 +16,9 @@ import net.minecraft.client.data.models.ItemModelGenerators;
 import net.minecraft.client.data.models.model.ItemModelUtils;
 import net.minecraft.client.data.models.model.ModelTemplates;
 import net.minecraft.client.data.models.model.TextureMapping;
+import net.minecraft.client.renderer.item.ItemModel;
 import net.minecraft.client.renderer.item.SelectItemModel;
+import net.minecraft.resources.Identifier;
 
 public class TinyFlowersItemModelProvider extends FabricModelProvider {
 	private final List<TinyFlowerData> flowers;
@@ -40,14 +43,23 @@ public class TinyFlowersItemModelProvider extends FabricModelProvider {
 				.filter(flowerData -> flowerData.shouldCreateItem())
 				.map(flowerData -> ItemModelUtils.when(
 						new TinyFlowerComponent(flowerData.id()),
-						ItemModelUtils.plainModel(ModelTemplates.FLAT_ITEM.create(
-								flowerData.id().withPrefix("item/"),
-								TextureMapping.layer0(flowerData.id().withPrefix("item/")),
-								itemModelGenerator.modelOutput))))
+						modelForIdentifier(flowerData.id(), itemModelGenerator)))
 				.collect(Collectors.toList());
 
 		itemModelGenerator.itemModelOutput.accept(ModItems.TINY_FLOWER_ITEM,
-				ItemModelUtils.select(new TinyFlowerProperty(), list));
+				ItemModelUtils.select(
+						new TinyFlowerProperty(),
+						modelForIdentifier(TinyFlowers.id("tiny_garden"), itemModelGenerator),
+						list));
+	}
+
+	private ItemModel.Unbaked modelForIdentifier(Identifier id, ItemModelGenerators itemModelGenerator) {
+		Identifier prefixed = id.withPrefix("item/");
+
+		return ItemModelUtils.plainModel(ModelTemplates.FLAT_ITEM.create(
+				prefixed,
+				TextureMapping.layer0(prefixed),
+				itemModelGenerator.modelOutput));
 	}
 
 	@Override
