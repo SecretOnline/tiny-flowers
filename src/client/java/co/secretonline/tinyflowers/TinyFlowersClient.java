a68e8f160909482bd9facb37e8ee8b3a461d460d
secret_online <me@secretonline.co>
2025-12-20 12:57:03 +1300

    Start block entity renderer for gardens

--- src/client/java/co/secretonline/tinyflowers/TinyFlowersClient.java
+++ src/client/java/co/secretonline/tinyflowers/TinyFlowersClient.java
@@ -1,11 +1,14 @@
 package co.secretonline.tinyflowers;
 
+import co.secretonline.tinyflowers.blocks.ModBlockEntities;
 import co.secretonline.tinyflowers.blocks.ModBlocks;
+import co.secretonline.tinyflowers.renderer.block.TinyGardenBlockEntityRenderer;
 import co.secretonline.tinyflowers.renderer.item.TinyFlowerProperty;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.minecraft.client.renderer.BiomeColors;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
 import net.minecraft.client.renderer.item.ItemStackRenderState;
 import net.minecraft.client.renderer.item.properties.select.SelectItemModelProperties;
@@ -22,6 +25,8 @@ public class TinyFlowersClient implements ClientModInitializer {
 		SelectItemModelProperties.ID_MAPPER.put(TinyFlowers.id("tiny_flower"), TinyFlowerProperty.TYPE);
 
 		BlockRenderLayerMap.putBlock(ModBlocks.TINY_GARDEN_BLOCK, ChunkSectionLayer.CUTOUT);
+
+		BlockEntityRenderers.register(ModBlockEntities.TINY_GARDEN_BLOCK_ENTITY, TinyGardenBlockEntityRenderer::new);
 
 		// See Pink Petals in net.minecraft.client.color.block.BlockColors
 		ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> {
