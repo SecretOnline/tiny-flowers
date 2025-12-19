72954328efd70829160ca4977312ee4a205d5c3e
secret_online <me@secretonline.co>
2025-12-19 16:23:47 +1300

    Fix a lot to do with item placement

--- src/main/java/co/secretonline/tinyflowers/helper/SegmentedMixinHelper.java
+++ src/main/java/co/secretonline/tinyflowers/helper/SegmentedMixinHelper.java
@@ -2,18 +2,26 @@ package co.secretonline.tinyflowers.helper;
 
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-// import co.secretonline.tinyflowers.TinyFlowers;
-import co.secretonline.tinyflowers.blocks.FlowerVariant;
-// import co.secretonline.tinyflowers.blocks.GardenBlock;
-// import co.secretonline.tinyflowers.blocks.ModBlocks;
+import co.secretonline.tinyflowers.TinyFlowers;
+import co.secretonline.tinyflowers.blocks.ModBlocks;
+import co.secretonline.tinyflowers.blocks.TinyGardenBlock;
+import co.secretonline.tinyflowers.blocks.TinyGardenBlockEntity;
+import co.secretonline.tinyflowers.data.TinyFlowerData;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SegmentableBlock;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.gameevent.GameEvent;
 
 /**
  * This class contains common logic for mixins targeting Segmented blocks.
@@ -39,8 +47,8 @@ public class SegmentedMixinHelper {
 			return;
 		}
 
-		FlowerVariant flowerVariant = FlowerVariant.fromItem(stack.getItem());
-		if (flowerVariant.isEmpty()) {
+		TinyFlowerData flowerData = TinyFlowerData.findByItemStack(context.getLevel().registryAccess(), stack);
+		if (flowerData == null) {
 			// The item is not a valid flower variant, so we don't need to do anything.
 			return;
 		}
@@ -50,9 +58,12 @@ public class SegmentedMixinHelper {
 
 	public static void getPlacementState(BlockPlaceContext context, Block blockBeingUsed, IntegerProperty amountProperty,
 			EnumProperty<Direction> directionProperty, CallbackInfoReturnable<BlockState> info) {
+		Level level = context.getLevel();
+		BlockPos blockPos = context.getClickedPos();
+
 		// We need to build a new BlockState if a Segmented block item is being placed
-		// inside a GardenBlock.
-		BlockState blockState = context.getLevel().getBlockState(context.getClickedPos());
+		// inside a TinyGardenBlock.
+		BlockState blockState = level.getBlockState(blockPos);
 		Block currentBlock = blockState.getBlock();
 
 		// Early exit if the block being placed is the same as the current block.
@@ -61,52 +72,70 @@ public class SegmentedMixinHelper {
 			return;
 		}
 
-		// // If the current block is not currently a garden but is able to be converted
-		// to
-		// // a FlowerVariant, then we need to convert the current blockstate to a
-		// // GardenBlock to before continuing.
-		// if (!(currentBlock instanceof GardenBlock)) {
-		// FlowerVariant currentBlockFlowerVariant =
-		// FlowerVariant.fromItem(currentBlock.asItem());
-		// if (!currentBlockFlowerVariant.isEmpty()) {
-		// try {
-		// blockState = ((GardenBlock)
-		// ModBlocks.TINY_GARDEN).getStateFromSegmented(blockState);
-		// currentBlock = blockState.getBlock();
-		// } catch (IllegalStateException e) {
-		// // This is expected to occur only if there are new Segmented blocks that
-		// don't
-		// // have tiny flowers. If the base game ever ends up doing this, then it's
-		// // probably woth handling this better. For now just spitting out a warning
-		// isn't
-		// // the worst thing.
-		// TinyFlowers.LOGGER.warn("Failed to convert blockstate to garden block.
-		// Ignoring", e);
-		// }
-		// }
-		// }
+		// If the current block is not currently a garden but is able to be converted to
+		// a tiny flower, then we need to convert the current blockstate to a
+		// TinyGardenBlock to before continuing.
+		if (!(currentBlock instanceof TinyGardenBlock)) {
+			TinyFlowerData flowerData = TinyFlowerData.findByOriginalBlock(level.registryAccess(), currentBlock);
+			if (flowerData != null) {
+				try {
+					BlockState prevBockState = blockState;
+					blockState = ((TinyGardenBlock) ModBlocks.TINY_GARDEN_BLOCK).defaultBlockState()
+							.setValue(TinyGardenBlock.FACING, blockState.getValue(BlockStateProperties.HORIZONTAL_FACING));
 
-		// if (currentBlock instanceof GardenBlock) {
-		// if (!GardenBlock.hasFreeSpace(blockState)) {
-		// // Can't add flower, so don't replace blockstate.
-		// // This case shouldn't ever be hit, as GardenBlock should have prevented
-		// // replacement.
-		// info.setReturnValue(blockState);
-		// return;
-		// }
+					// Since we also need to update the entity, try to update the world now.
+					level.setBlockAndUpdate(blockPos, blockState);
 
-		// // There's space in the garden, so add a flower.
-		// FlowerVariant flowerVariant = FlowerVariant.fromItem(blockBeingUsed);
-		// if (flowerVariant.isEmpty()) {
-		// // Is this the correct thing to do?
-		// // Do we need to do anything to prevent the item from being consumed?
-		// info.setReturnValue(blockState);
-		// return;
-		// }
+					if (!(level.getBlockEntity(blockPos) instanceof TinyGardenBlockEntity gardenBlockEntity)) {
+						// If there's no block entity, try undo the change
+						level.setBlockAndUpdate(blockPos, prevBockState);
+						return;
+					}
 
-		// BlockState newState = GardenBlock.addFlowerToBlockState(blockState,
-		// flowerVariant);
-		// info.setReturnValue(newState);
-		// }
+					gardenBlockEntity.setFromPreviousBlockState(level.registryAccess(), prevBockState);
+
+					currentBlock = blockState.getBlock();
+				} catch (IllegalStateException e) {
+					// This is expected to occur only if there are new Segmented blocks that don't
+					// have tiny flowers. If the base game ever ends up doing this, then it's
+					// probably woth handling this better. For now just spitting out a warning isn't
+					// the worst thing.
+					TinyFlowers.LOGGER.warn("Failed to convert blockstate to garden block. Ignoring", e);
+				}
+			}
+		}
+
+		if (currentBlock instanceof TinyGardenBlock) {
+			if (!(level.getBlockEntity(blockPos) instanceof TinyGardenBlockEntity gardenBlockEntity)) {
+				return;
+			}
+			if (gardenBlockEntity.isFull()) {
+				// Can't add flower, so don't replace blockstate.
+				// This case shouldn't ever be hit, as TinyGardenBlock should have prevented
+				// replacement.
+				info.setReturnValue(blockState);
+				return;
+			}
+
+			// There's space in the garden, so add a flower.
+			TinyFlowerData flowerData = TinyFlowerData.findByOriginalBlock(level.registryAccess(),
+					blockBeingUsed);
+			if (flowerData == null) {
+				info.setReturnValue(blockState);
+				return;
+			}
+
+			gardenBlockEntity.addFlower(flowerData.id());
+
+			// Consume item, play sound, and send game event.
+			Player player = context.getPlayer();
+			SoundType soundType = blockState.getSoundType();
+			level.playSound(player, blockPos, soundType.getPlaceSound(), SoundSource.BLOCKS,
+					(soundType.getVolume() + 1.0F) / 2.0F, soundType.getPitch() * 0.8F);
+			level.gameEvent(GameEvent.BLOCK_PLACE, blockPos, GameEvent.Context.of(player, blockState));
+			context.getItemInHand().consume(1, player);
+
+			info.setReturnValue(blockState);
+		}
 	}
 }
