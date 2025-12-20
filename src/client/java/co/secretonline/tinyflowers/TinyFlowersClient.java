113404ae6c76cb4d4b683dd566ad4d6449914b8e
secret_online <me@secretonline.co>
2025-12-21 12:13:54 +1300

    Switch to model loading plugin to load models

--- src/client/java/co/secretonline/tinyflowers/TinyFlowersClient.java
+++ src/client/java/co/secretonline/tinyflowers/TinyFlowersClient.java
@@ -4,17 +4,17 @@ import co.secretonline.tinyflowers.blocks.ModBlockEntities;
 import co.secretonline.tinyflowers.blocks.ModBlocks;
 import co.secretonline.tinyflowers.renderer.block.TinyGardenBlockEntityRenderer;
 import co.secretonline.tinyflowers.renderer.item.TinyFlowerProperty;
-import co.secretonline.tinyflowers.resources.TinyFlowerModelReloadListener;
+import co.secretonline.tinyflowers.resources.TinyFlowerModelDataLoader;
+import co.secretonline.tinyflowers.resources.TinyFlowersModelLoadingPlugin;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
-import net.fabricmc.fabric.api.resource.v1.ResourceLoader;
+import net.fabricmc.fabric.impl.client.model.loading.ModelLoadingPluginManager;
 import net.minecraft.client.renderer.BiomeColors;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
 import net.minecraft.client.renderer.item.ItemStackRenderState;
 import net.minecraft.client.renderer.item.properties.select.SelectItemModelProperties;
-import net.minecraft.server.packs.PackType;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.DryFoliageColor;
 import net.minecraft.world.level.GrassColor;
@@ -31,23 +31,22 @@ public class TinyFlowersClient implements ClientModInitializer {
 
 		BlockEntityRenderers.register(ModBlockEntities.TINY_GARDEN_BLOCK_ENTITY, TinyGardenBlockEntityRenderer::new);
 
-		ResourceLoader.get(PackType.CLIENT_RESOURCES).registerReloader(
-				TinyFlowers.id("models"), new TinyFlowerModelReloadListener());
+		ModelLoadingPluginManager.registerPlugin(new TinyFlowerModelDataLoader(), new TinyFlowersModelLoadingPlugin());
 
 		// See Pink Petals in net.minecraft.client.color.block.BlockColors
 		ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> {
-			boolean hasWorld = world == null || pos == null;
+			boolean useDefaultColors = world == null || pos == null;
 
 			switch (tintIndex) {
 				case 1 -> {
-					if (hasWorld) {
+					if (useDefaultColors) {
 						return GrassColor.getDefaultColor();
 					} else {
 						return BiomeColors.getAverageGrassColor(world, pos);
 					}
 				}
 				case 2 -> {
-					if (hasWorld) {
+					if (useDefaultColors) {
 						return DryFoliageColor.get(0.5, 1.0);
 					} else {
 						return BiomeColors.getAverageDryFoliageColor(world, pos);
---
+++ src/client/java/co/secretonline/tinyflowers/resources/TinyFlowersModelLoadingPlugin.java
@@ -0,0 +1,42 @@
+package co.secretonline.tinyflowers.resources;
+
+import java.util.Map;
+
+import co.secretonline.tinyflowers.items.ModItems;
+import net.fabricmc.fabric.api.client.model.loading.v1.ExtraModelKey;
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin.Context;
+import net.fabricmc.fabric.api.client.model.loading.v1.PreparableModelLoadingPlugin;
+import net.fabricmc.fabric.api.client.model.loading.v1.SimpleUnbakedExtraModel;
+import net.minecraft.resources.Identifier;
+
+public class TinyFlowersModelLoadingPlugin
+		implements PreparableModelLoadingPlugin<Map<Identifier, TinyFlowerResources>> {
+
+	@Override
+	public void initialize(Map<Identifier, TinyFlowerResources> data, Context pluginContext) {
+		for (var entry : data.entrySet()) {
+			TinyFlowerResources resources = entry.getValue();
+
+			pluginContext.addModel(
+					ExtraModelKey.create(resources.modelPart1()::toString),
+					SimpleUnbakedExtraModel.blockStateModel(resources.modelPart1()));
+			pluginContext.addModel(
+					ExtraModelKey.create(resources.modelPart2()::toString),
+					SimpleUnbakedExtraModel.blockStateModel(resources.modelPart2()));
+			pluginContext.addModel(
+					ExtraModelKey.create(resources.modelPart3()::toString),
+					SimpleUnbakedExtraModel.blockStateModel(resources.modelPart3()));
+			pluginContext.addModel(
+					ExtraModelKey.create(resources.modelPart4()::toString),
+					SimpleUnbakedExtraModel.blockStateModel(resources.modelPart4()));
+		}
+
+		pluginContext.modifyItemModelBeforeBake().register((model, itemContext) -> {
+			if (itemContext.itemId().equals(ModItems.TINY_FLOWER_ITEM_KEY.identifier())) {
+			}
+
+			return model;
+		});
+	}
+
+}
