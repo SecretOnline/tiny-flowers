e0bdb2706f9c4ec90a82db188fd9f7b6463a2312
secret_online <me@secretonline.co>
2025-12-21 13:33:09 +1300

    Use model manager to get block models in block entity renderer

--- src/client/java/co/secretonline/tinyflowers/renderer/block/TinyGardenBlockEntityRenderer.java
+++ src/client/java/co/secretonline/tinyflowers/renderer/block/TinyGardenBlockEntityRenderer.java
@@ -1,6 +1,5 @@
 package co.secretonline.tinyflowers.renderer.block;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
@@ -12,48 +11,24 @@ import com.mojang.math.Axis;
 import co.secretonline.tinyflowers.blocks.TinyGardenBlock;
 import co.secretonline.tinyflowers.blocks.TinyGardenBlockEntity;
 import co.secretonline.tinyflowers.resources.TinyFlowerResources;
-import net.minecraft.client.gui.Font;
-import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.fabricmc.fabric.api.client.model.loading.v1.ExtraModelKey;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.feature.ModelFeatureRenderer.CrumblingOverlay;
+import net.minecraft.client.renderer.rendertype.RenderTypes;
 import net.minecraft.client.renderer.state.CameraRenderState;
+import net.minecraft.client.resources.model.ModelManager;
 import net.minecraft.core.Direction;
-import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.phys.Vec3;
 
 public class TinyGardenBlockEntityRenderer
 		implements BlockEntityRenderer<TinyGardenBlockEntity, TinyGardenBlockEntityRenderState> {
-	private final Map<Identifier, BakedModelHolder> modelsForVariant = new HashMap<>();
 
 	public TinyGardenBlockEntityRenderer(BlockEntityRendererProvider.Context context) {
-		// This map is re-created when resources are reloaded. Luckily, a new instance
-		// of this class is made too.
-		Map<Identifier, TinyFlowerResources> resourceMap = TinyFlowerResources.getInstances();
-
-		// Some primitive caching for model baking, since most flowers shoudl use the
-		// same models.
-		Map<Identifier, ModelPart> bakedModels = new HashMap<>();
-		// resourceMap.forEach((id, resource) -> {
-		// for (var part : new TinyFlowerResources.Part[] { resource.part1(),
-		// resource.part2(),
-		// resource.part3(), resource.part4() }) {
-		// if (!bakedModels.containsKey(part.model())) {
-		// var location = new ModelLayerLocation(part.model(), "main");
-		// var baked = context.bakeLayer(location);
-		// bakedModels.put(part.model(), baked);
-		// }
-		// }
-
-		// modelsForVariant.put(id, new BakedModelHolder(
-		// bakedModels.get(resource.part1().model()), resource.part1().textures(),
-		// bakedModels.get(resource.part2().model()), resource.part2().textures(),
-		// bakedModels.get(resource.part3().model()), resource.part3().textures(),
-		// bakedModels.get(resource.part4().model()), resource.part4().textures()));
-		// });
 	}
 
 	@Override
@@ -84,66 +59,75 @@ public class TinyGardenBlockEntityRenderer
 		float rotationDegrees = Direction.getYRot(blockEntityRenderState.getDirection());
 		poseStack.mulPose(Axis.YP.rotationDegrees(rotationDegrees));
 
-		if (blockEntityRenderState.getFlower1() != null) {
-			poseStack.pushPose();
-			poseStack.scale(1 / 32f, 1 / 32f, 1 / 32f);
-			poseStack.mulPose(Axis.XP.rotationDegrees(180));
-			submitNodeCollector.submitText(poseStack, 24f, -20f,
-					Component.literal(blockEntityRenderState.getFlower1().toString()).getVisualOrderText(),
-					false,
-					Font.DisplayMode.SEE_THROUGH,
-					blockEntityRenderState.lightCoords,
-					0xffffffff,
-					1,
-					0);
-			poseStack.popPose();
-		}
-		if (blockEntityRenderState.getFlower2() != null) {
-			poseStack.pushPose();
-			poseStack.scale(1 / 32f, 1 / 32f, 1 / 32f);
-			poseStack.mulPose(Axis.XP.rotationDegrees(180));
-			poseStack.translate(0, 0, 32);
-			submitNodeCollector.submitText(poseStack, 24f, -20f,
-					Component.literal(blockEntityRenderState.getFlower2().toString()).getVisualOrderText(),
-					false,
-					Font.DisplayMode.SEE_THROUGH,
-					blockEntityRenderState.lightCoords,
-					0xffffffff,
-					0,
-					0);
-			poseStack.popPose();
-		}
-		if (blockEntityRenderState.getFlower3() != null) {
-			poseStack.pushPose();
-			poseStack.scale(1 / 32f, 1 / 32f, 1 / 32f);
-			poseStack.mulPose(Axis.XP.rotationDegrees(180));
-			poseStack.translate(0, 0, 32);
-			submitNodeCollector.submitText(poseStack, 8f, -10f,
-					Component.literal(blockEntityRenderState.getFlower3().toString()).getVisualOrderText(),
-					false,
-					Font.DisplayMode.SEE_THROUGH,
-					blockEntityRenderState.lightCoords,
-					0xffffffff,
-					0,
-					0);
-			poseStack.popPose();
-		}
-		if (blockEntityRenderState.getFlower4() != null) {
-			poseStack.pushPose();
-			poseStack.scale(1 / 32f, 1 / 32f, 1 / 32f);
-			poseStack.mulPose(Axis.XP.rotationDegrees(180));
-			submitNodeCollector.submitText(poseStack, 8f, -10f,
-					Component.literal(blockEntityRenderState.getFlower4().toString()).getVisualOrderText(),
-					false,
-					Font.DisplayMode.SEE_THROUGH,
-					blockEntityRenderState.lightCoords,
-					0xffffffff,
-					0,
-					0);
-			poseStack.popPose();
-		}
+		submitPartForFlowerIndex(blockEntityRenderState, poseStack, submitNodeCollector, 1);
+		submitPartForFlowerIndex(blockEntityRenderState, poseStack, submitNodeCollector, 2);
+		submitPartForFlowerIndex(blockEntityRenderState, poseStack, submitNodeCollector, 3);
+		submitPartForFlowerIndex(blockEntityRenderState, poseStack, submitNodeCollector, 4);
 
 		poseStack.popPose();
+	}
+
+	private void submitPartForFlowerIndex(TinyGardenBlockEntityRenderState blockEntityRenderState, PoseStack poseStack,
+			SubmitNodeCollector submitNodeCollector, int index) {
+		Identifier id;
+		switch (index) {
+			case 1:
+				id = blockEntityRenderState.getFlower1();
+				break;
+			case 2:
+				id = blockEntityRenderState.getFlower2();
+				break;
+			case 3:
+				id = blockEntityRenderState.getFlower3();
+				break;
+			case 4:
+				id = blockEntityRenderState.getFlower4();
+				break;
+			default:
+				throw new IllegalArgumentException("Invalid flower index " + index);
+		}
+		if (id == null) {
+			return;
+		}
+
+		Map<Identifier, TinyFlowerResources> resourceMap = TinyFlowerResources.getInstances();
+		TinyFlowerResources resources = resourceMap.get(id);
+		if (resources == null) {
+			return;
+		}
+
+		Identifier modelId;
+		switch (index) {
+			case 1:
+				modelId = resources.modelPart1();
+				break;
+			case 2:
+				modelId = resources.modelPart2();
+				break;
+			case 3:
+				modelId = resources.modelPart3();
+				break;
+			case 4:
+				modelId = resources.modelPart4();
+				break;
+			default:
+				throw new IllegalArgumentException("Invalid flower index " + index);
+		}
+		if (modelId == null) {
+			return;
+		}
+
+		ExtraModelKey<ModelPart> key = ExtraModelKey.create(modelId::toString);
+
+		Minecraft minecraft = Minecraft.getInstance();
+		ModelManager modelManager = minecraft.getModelManager();
+
+		ModelPart model = modelManager.getModel(key);
+		if (model == null) {
+			return;
+		}
+
+		submitNodeCollector.submitModelPart(model, poseStack, RenderTypes.cutoutMovingBlock(), 0, 0, null);
 	}
 
 	@Override
@@ -153,11 +137,5 @@ public class TinyGardenBlockEntityRenderer
 		// bit sad if distant gardens aren't rendered in. Expecially since these are
 		// mean to be part of the world, which usually doesn't distance culling.
 		return 256;
-	}
-
-	private static record BakedModelHolder(ModelPart part1, Map<String, Identifier> textures1,
-			ModelPart part2, Map<String, Identifier> textures2,
-			ModelPart part3, Map<String, Identifier> textures3,
-			ModelPart part4, Map<String, Identifier> textures4) {
 	}
 }
