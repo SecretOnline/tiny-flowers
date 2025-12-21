f34fb35074bee1051412127d36c5c99de072adcd
secret_online <me@secretonline.co>
2025-12-21 21:21:54 +1300

    Load item models dynamically

--- src/client/java/co/secretonline/tinyflowers/resources/TinyFlowersModelLoadingPlugin.java
+++ src/client/java/co/secretonline/tinyflowers/resources/TinyFlowersModelLoadingPlugin.java
@@ -1,11 +1,22 @@
 package co.secretonline.tinyflowers.resources;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import co.secretonline.tinyflowers.TinyFlowers;
+import co.secretonline.tinyflowers.components.TinyFlowerComponent;
 import co.secretonline.tinyflowers.items.ModItems;
+import co.secretonline.tinyflowers.renderer.item.TinyFlowerProperty;
 import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin.Context;
 import net.fabricmc.fabric.api.client.model.loading.v1.PreparableModelLoadingPlugin;
 import net.fabricmc.fabric.api.client.model.loading.v1.SimpleUnbakedExtraModel;
+import net.minecraft.client.data.models.model.ItemModelUtils;
+import net.minecraft.client.data.models.model.ModelInstance;
+import net.minecraft.client.data.models.model.ModelTemplates;
+import net.minecraft.client.data.models.model.TextureMapping;
+import net.minecraft.client.renderer.item.ItemModel;
+import net.minecraft.client.renderer.item.SelectItemModel;
 import net.minecraft.resources.Identifier;
 
 public class TinyFlowersModelLoadingPlugin
@@ -14,6 +25,8 @@ public class TinyFlowersModelLoadingPlugin
 	@Override
 	public void initialize(Map<Identifier, TinyFlowerResolvedResources> data, Context pluginContext) {
 		TinyFlowerResolvedResources.setInstances(data);
+
+		List<SelectItemModel.SwitchCase<TinyFlowerComponent>> itemModels = new ArrayList<>();
 
 		for (var entry : data.entrySet()) {
 			TinyFlowerResolvedResources resources = entry.getValue();
@@ -30,14 +43,31 @@ public class TinyFlowersModelLoadingPlugin
 			pluginContext.addModel(
 					resources.model4().extraModelKey(),
 					SimpleUnbakedExtraModel.blockStateModel(resources.model4().modelId()));
+
+			itemModels.add(ItemModelUtils.when(
+					new TinyFlowerComponent(resources.id()),
+					modelForIdentifier(resources.itemTexture())));
 		}
 
 		pluginContext.modifyItemModelBeforeBake().register((model, itemContext) -> {
 			if (itemContext.itemId().equals(ModItems.TINY_FLOWER_ITEM_KEY.identifier())) {
+				return ItemModelUtils.select(
+						new TinyFlowerProperty(),
+						modelForIdentifier(TinyFlowers.id("item/tiny_garden")),
+						itemModels);
 			}
 
 			return model;
 		});
 	}
 
+	private ItemModel.Unbaked modelForIdentifier(Identifier id) {
+		return ItemModelUtils.plainModel(ModelTemplates.FLAT_ITEM.create(
+				id,
+				TextureMapping.layer0(id),
+				this::consumeModel));
+	}
+
+	private void consumeModel(Identifier id, ModelInstance model) {
+	}
 }
