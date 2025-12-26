62cedf685640116215fb750e3bb094503c987135
secret_online <me@secretonline.co>
2025-12-26 15:49:12 +1300

    Add Tiny Flowers tab to Creative menu
    After a very quick test, it seems like recipe viewers don't like this mod too much. It seems fixing that will mean depending on their API packages. which I'm trying to avoid for no real reason. The excuse I have is that it'll likely slow down updating the mod to new versions, especially if I need different implementations for JEI, REI, EMI, and perhaps others.

--- src/client/java/co/secretonline/tinyflowers/TinyFlowersClient.java
+++ src/client/java/co/secretonline/tinyflowers/TinyFlowersClient.java
@@ -2,7 +2,9 @@ package co.secretonline.tinyflowers;
 
 import co.secretonline.tinyflowers.blocks.ModBlockEntities;
 import co.secretonline.tinyflowers.blocks.ModBlocks;
+import co.secretonline.tinyflowers.items.CreativeTabEvents;
 import co.secretonline.tinyflowers.renderer.block.TinyGardenBlockEntityRenderer;
+import co.secretonline.tinyflowers.renderer.block.TinyGardenColorProvider;
 import co.secretonline.tinyflowers.renderer.item.TinyFlowerProperty;
 import co.secretonline.tinyflowers.resources.TinyFlowerModelDataLoader;
 import co.secretonline.tinyflowers.resources.TinyFlowersModelLoadingPlugin;
@@ -10,14 +12,11 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.fabricmc.fabric.impl.client.model.loading.ModelLoadingPluginManager;
-import net.minecraft.client.renderer.BiomeColors;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
 import net.minecraft.client.renderer.item.ItemStackRenderState;
 import net.minecraft.client.renderer.item.properties.select.SelectItemModelProperties;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.DryFoliageColor;
-import net.minecraft.world.level.GrassColor;
 
 public class TinyFlowersClient implements ClientModInitializer {
 	public static final RandomSource RANDOM = RandomSource.create();
@@ -33,30 +32,8 @@ public class TinyFlowersClient implements ClientModInitializer {
 
 		ModelLoadingPluginManager.registerPlugin(new TinyFlowerModelDataLoader(), new TinyFlowersModelLoadingPlugin());
 
-		// See Pink Petals in net.minecraft.client.color.block.BlockColors
-		ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> {
-			boolean useDefaultColors = world == null || pos == null;
+		CreativeTabEvents.initialize();
 
-			switch (tintIndex) {
-				case 1 -> {
-					if (useDefaultColors) {
-						return GrassColor.getDefaultColor();
-					} else {
-						return BiomeColors.getAverageGrassColor(world, pos);
-					}
-				}
-				case 2 -> {
-					if (useDefaultColors) {
-						return DryFoliageColor.get(0.5, 1.0);
-					} else {
-						return BiomeColors.getAverageDryFoliageColor(world, pos);
-					}
-				}
-				default -> {
-					return -1;
-				}
-			}
-		}, ModBlocks.TINY_GARDEN_BLOCK);
-
+		ColorProviderRegistry.BLOCK.register(new TinyGardenColorProvider(), ModBlocks.TINY_GARDEN_BLOCK);
 	}
 }
---
+++ src/client/java/co/secretonline/tinyflowers/renderer/block/TinyGardenColorProvider.java
@@ -0,0 +1,42 @@
+package co.secretonline.tinyflowers.renderer.block;
+
+import org.jspecify.annotations.Nullable;
+
+import net.minecraft.client.color.block.BlockColor;
+import net.minecraft.client.renderer.BiomeColors;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.DryFoliageColor;
+import net.minecraft.world.level.GrassColor;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class TinyGardenColorProvider implements BlockColor {
+
+	@Override
+	public int getColor(BlockState state, @Nullable BlockAndTintGetter world, @Nullable BlockPos pos, int tintIndex) {
+		// Loosely based on Pink Petals in net.minecraft.client.color.block.BlockColors
+
+		boolean useDefaultColors = world == null || pos == null;
+
+		switch (tintIndex) {
+			case 1 -> {
+				if (useDefaultColors) {
+					return GrassColor.getDefaultColor();
+				} else {
+					return BiomeColors.getAverageGrassColor(world, pos);
+				}
+			}
+			case 2 -> {
+				if (useDefaultColors) {
+					return DryFoliageColor.get(0.5, 1.0);
+				} else {
+					return BiomeColors.getAverageDryFoliageColor(world, pos);
+				}
+			}
+			default -> {
+				return -1;
+			}
+		}
+	}
+
+}
