b95ae5738ba1f44fc7b070ec8fc9f3e639c5619c
secret_online <me@secretonline.co>
2025-12-31 12:26:16 +1300

    Add more block tags to the Tiny Garden block

--- src/client/java/co/secretonline/tinyflowers/datagen/providers/BlockTagProvider.java
+++ src/client/java/co/secretonline/tinyflowers/datagen/providers/BlockTagProvider.java
@@ -6,6 +6,7 @@ import co.secretonline.tinyflowers.blocks.ModBlockTags;
 import co.secretonline.tinyflowers.blocks.ModBlocks;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.fabricmc.fabric.api.tag.convention.v2.ConventionalBlockTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Blocks;
@@ -18,6 +19,9 @@ public class BlockTagProvider extends FabricTagProvider.FabricValueLookupTagProv
 	@Override
 	protected void addTags(HolderLookup.Provider wrapperLookup) {
 		valueLookupBuilder(BlockTags.FLOWERS).add(ModBlocks.TINY_GARDEN_BLOCK);
+		valueLookupBuilder(BlockTags.BEE_ATTRACTIVE).add(ModBlocks.TINY_GARDEN_BLOCK);
+		valueLookupBuilder(BlockTags.INSIDE_STEP_SOUND_BLOCKS).add(ModBlocks.TINY_GARDEN_BLOCK);
+		valueLookupBuilder(ConventionalBlockTags.FLOWERS).add(ModBlocks.TINY_GARDEN_BLOCK);
 
 		valueLookupBuilder(ModBlockTags.TINY_FLOWER_CAN_SURVIVE_ON).forceAddTag(BlockTags.DIRT);
 		valueLookupBuilder(ModBlockTags.TINY_FLOWER_CAN_SURVIVE_ON).add(Blocks.FARMLAND);
