6ab1333c4beac4f0222d64936cdb51faa427ddbb
secret_online <me@secretonline.co>
2026-01-01 12:02:51 +1300

    Make eyeblossom behaviour data-driven

--- src/main/java/co/secretonline/tinyflowers/helper/EyeblossomHelper.java
+++ src/main/java/co/secretonline/tinyflowers/helper/EyeblossomHelper.java
@@ -1,35 +1,84 @@
 package co.secretonline.tinyflowers.helper;
 
-import co.secretonline.tinyflowers.TinyFlowers;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jetbrains.annotations.Nullable;
+
 import co.secretonline.tinyflowers.blocks.ModBlocks;
 import co.secretonline.tinyflowers.blocks.TinyGardenBlockEntity;
-import co.secretonline.tinyflowers.mixin.EyeblossomStateAccessor;
+import co.secretonline.tinyflowers.data.TinyFlowerData;
+import co.secretonline.tinyflowers.data.special.EyeblossomOpeningSpecialFeature;
+import co.secretonline.tinyflowers.data.special.SpecialFeature;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.Identifier;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
 import net.minecraft.util.TriState;
+import net.minecraft.util.Util;
 import net.minecraft.world.attribute.EnvironmentAttributes;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.EyeblossomBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
 
 public class EyeblossomHelper {
-	private static final Identifier OPEN_EYEBLOSSOM = TinyFlowers.id("tiny_open_eyeblossom");
-	private static final Identifier CLOSED_EYEBLOSSOM = TinyFlowers.id("tiny_closed_eyeblossom");
 
-	public static Identifier getTinyFlowerIdForOpenState(boolean isOpen) {
-		return isOpen
-				? OPEN_EYEBLOSSOM
-				: CLOSED_EYEBLOSSOM;
-	}
+	public static boolean doEyeblossomTick(BlockState state, ServerLevel world, BlockPos pos, RandomSource random,
+			boolean isRandomTick) {
+		TriState openTriState = world.environmentAttributes().getValue(EnvironmentAttributes.EYEBLOSSOM_OPEN, pos);
+		if (openTriState == TriState.DEFAULT) {
+			return false;
+		}
 
-	public static Block getEyeblossomBlockForOpenState(boolean isOpen) {
-		return isOpen
-				? Blocks.OPEN_EYEBLOSSOM
-				: Blocks.CLOSED_EYEBLOSSOM;
+		BlockState currentState = state;
+		boolean didChange = false;
+
+		if (!(world.getBlockEntity(pos) instanceof TinyGardenBlockEntity gardenBlockEntity)) {
+			// If there's no block entity, don't do anything
+			return false;
+		}
+
+		List<EyeblossomOpeningSpecialFeature> allChanges = new ArrayList<>();
+
+		for (int i = 1; i <= 4; i++) {
+			@Nullable
+			Identifier flowerId = gardenBlockEntity.getFlower(i);
+			if (flowerId == null) {
+				continue;
+			}
+
+			@Nullable
+			TinyFlowerData flowerData = TinyFlowerData.findById(world.registryAccess(), flowerId);
+			if (flowerData == null) {
+				continue;
+			}
+
+			for (SpecialFeature feature : flowerData.specialFeatures()) {
+				if (feature instanceof EyeblossomOpeningSpecialFeature eyeblossomFeature) {
+					if (eyeblossomFeature.when().shouldChange(openTriState)) {
+						gardenBlockEntity.setFlower(i, eyeblossomFeature.turnsInto());
+						allChanges.add(eyeblossomFeature);
+
+						// Stop processing special features.
+						break;
+					}
+				}
+			}
+		}
+
+		if (allChanges.size() > 0) {
+			world.setBlock(pos, currentState, Block.UPDATE_CLIENTS);
+			world.gameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Context.of(state));
+
+			EyeblossomHelper.notifyNearbyEyeblossoms(state, world, pos, random);
+
+			EyeblossomOpeningSpecialFeature randomChange = Util.getRandom(allChanges, random);
+			randomChange.spawnTransformParticle(world, pos, random);
+			randomChange.playSound(world, pos, isRandomTick);
+		}
+
+		return didChange;
 	}
 
 	public static void notifyNearbyEyeblossoms(BlockState state, ServerLevel world, BlockPos pos, RandomSource random) {
@@ -38,23 +87,22 @@ public class EyeblossomHelper {
 			return;
 		}
 
-		boolean shouldOpen = openTriState.toBoolean(true);
-		Identifier incorrectId = getTinyFlowerIdForOpenState(!shouldOpen);
-		Block incorrectEyeblossom = getEyeblossomBlockForOpenState(!shouldOpen);
+		// This is to detect whether the block requesting this notification is an actual
+		// Eyeblossom. Actual Eyeblossoms will have already done this loop for
+		// Eyeblossom blocks, so if that's the case then we only need to notify gardens.
+		// If the origin was not an Eyeblossom, then we need to notify both Eyeblossoms
+		// and Tiny Gardens.
+		boolean originIsEyeblossom = state.is(Blocks.CLOSED_EYEBLOSSOM) || state.is(Blocks.OPEN_EYEBLOSSOM);
 
-		// This is to detect whether the central block of the area is a real Eyeblossom
-		// flower.
-		// This method gets called in two places:
-		// 1. In GardenBlock, where we want to notify Eyeblossoms and gardens.
-		// 2. In EyeblossomBlockMixin, which injects after Eyeblossoms are already
-		// notified so we don't want to double up the scheduled ticks.
-		boolean blockIsEyeblossom = state.is(Blocks.CLOSED_EYEBLOSSOM) || state.is(Blocks.OPEN_EYEBLOSSOM);
+		// Calculate what the wrong eyeblossom is so we don't have to do it every time
+		// in the loop.
+		Block incorrectEyeblossom = openTriState.toBoolean(true) ? Blocks.CLOSED_EYEBLOSSOM : Blocks.OPEN_EYEBLOSSOM;
 
 		BlockPos.betweenClosed(pos.offset(-3, -2, -3), pos.offset(3, 2, 3)).forEach(otherPos -> {
 			BlockState nearbyBlockState = world.getBlockState(otherPos);
 
-			// Eyeblossoms
-			if (!blockIsEyeblossom && nearbyBlockState.is(incorrectEyeblossom)) {
+			// Update Eyeblossoms if the source was not an eyeblossom.
+			if (nearbyBlockState.is(incorrectEyeblossom) && !originIsEyeblossom) {
 				scheduleBlockTick(world, pos, otherPos, incorrectEyeblossom, random);
 				return;
 			}
@@ -68,10 +116,25 @@ public class EyeblossomHelper {
 				}
 
 				// Tiny Gardens should also recieve updates if they have eyeblossoms.
-				for (Identifier gardenFlowerId : gardenBlockEntity.getFlowers()) {
-					if (gardenFlowerId.equals(incorrectId)) {
-						scheduleBlockTick(world, pos, otherPos, ModBlocks.TINY_GARDEN_BLOCK, random);
-						return;
+				boolean didNotify = false;
+				for (Identifier flowerId : gardenBlockEntity.getFlowers()) {
+					@Nullable
+					TinyFlowerData flowerData = TinyFlowerData.findById(world.registryAccess(), flowerId);
+					if (flowerData == null) {
+						continue;
+					}
+
+					for (SpecialFeature feature : flowerData.specialFeatures()) {
+						if (feature instanceof EyeblossomOpeningSpecialFeature eyeblossomFeature) {
+							if (eyeblossomFeature.when().shouldChange(openTriState)) {
+								scheduleBlockTick(world, pos, otherPos, ModBlocks.TINY_GARDEN_BLOCK, random);
+								didNotify = true;
+								break;
+							}
+						}
+					}
+					if (didNotify) {
+						break;
 					}
 				}
 			}
@@ -85,17 +148,5 @@ public class EyeblossomHelper {
 				10.0));
 
 		world.scheduleTick(otherPos, block, numTicks);
-	}
-
-	public static void playSound(ServerLevel world, BlockPos pos, boolean isOpen, boolean isLong) {
-		EyeblossomBlock.Type state = EyeblossomBlock.Type.fromBoolean(isOpen);
-
-		if (isLong) {
-			world.playSound(null, pos, state.longSwitchSound(),
-					SoundSource.BLOCKS, 1.0F, 1.0F);
-		} else {
-			world.playSound(null, pos, ((EyeblossomStateAccessor) ((Object) (state))).getShortSwitchSound(),
-					SoundSource.BLOCKS, 1.0F, 1.0F);
-		}
 	}
 }
