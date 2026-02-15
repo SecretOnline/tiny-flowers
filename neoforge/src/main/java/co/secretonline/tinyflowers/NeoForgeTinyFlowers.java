140e6ba418027033f48a4f6a20dbae9e25c7cb6a
secret_online <me@secretonline.co>
2026-02-15 18:43:57 +1300

    Add creative tab to NeoForge

--- neoforge/src/main/java/co/secretonline/tinyflowers/NeoForgeTinyFlowers.java
+++ neoforge/src/main/java/co/secretonline/tinyflowers/NeoForgeTinyFlowers.java
@@ -1,15 +1,14 @@
 package co.secretonline.tinyflowers;
 
-import co.secretonline.tinyflowers.block.entity.ModBlockEntities;
 import co.secretonline.tinyflowers.block.ModBlocks;
-import co.secretonline.tinyflowers.item.component.ModComponents;
+import co.secretonline.tinyflowers.block.entity.ModBlockEntities;
 import co.secretonline.tinyflowers.data.ModRegistries;
 import co.secretonline.tinyflowers.data.TinyFlowerData;
 import co.secretonline.tinyflowers.item.ModItems;
+import co.secretonline.tinyflowers.item.component.ModComponents;
 import co.secretonline.tinyflowers.item.crafting.ModRecipeSerializers;
 import co.secretonline.tinyflowers.platform.NeoForgeRegistryHelper;
 import co.secretonline.tinyflowers.platform.Services;
-import co.secretonline.tinyflowers.platform.RegistryHelper;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
@@ -20,19 +19,20 @@ import net.neoforged.neoforge.registries.DataPackRegistryEvent;
 @Mod(value = TinyFlowers.MOD_ID)
 @EventBusSubscriber(modid = TinyFlowers.MOD_ID)
 public class NeoForgeTinyFlowers {
-	public NeoForgeTinyFlowers(ModContainer container, IEventBus modBus) {
+	public NeoForgeTinyFlowers(IEventBus modBus) {
 		ModBlocks.initialize();
 		ModBlockEntities.initialize();
 		ModComponents.initialize();
 		ModItems.initialize();
 		ModRecipeSerializers.initialize();
 
-		RegistryHelper registryHelper = Services.REGISTRY;
-		if (registryHelper instanceof NeoForgeRegistryHelper neoForgeRegistryHelper) {
+		if (Services.REGISTRY instanceof NeoForgeRegistryHelper neoForgeRegistryHelper) {
 			neoForgeRegistryHelper.registerToBus(modBus);
 		} else {
 			throw new NullPointerException("Registry helper was not for NeoForge");
 		}
+
+		NeoForgeCreativeTabHandler.registerToBus(modBus);
 	}
 
 	@SubscribeEvent
